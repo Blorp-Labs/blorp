2291224e7b398f6aee46de07d9c5bb601d5d6bb8
Christian <5768801+christianjuth@users.noreply.github.com>
2025-09-16 08:17:04 -0400

    refactor: dial back the easter eggs a little

--- src/features/easter-eggs/confetti.ts
+++ src/features/easter-eggs/confetti.ts
@@ -7,15 +7,9 @@ const scalar = 2;
 const getShapes = _.memoize(() => {
   const cake = canvasConfetti.shapeFromText({ text: "🎂", scalar });
   const party_face = canvasConfetti.shapeFromText({ text: "🥳", scalar });
-  const mouse = canvasConfetti.shapeFromText({ text: "🐁", scalar });
-  const pie = canvasConfetti.shapeFromText({ text: "🥧", scalar });
-  const penguin = canvasConfetti.shapeFromText({ text: "🐧", scalar });
   return {
     cake,
     party_face,
-    mouse,
-    pie,
-    penguin,
   };
 });
 
@@ -39,15 +33,7 @@ export function confetti(text: string) {
   if (text.includes("happycakeday")) {
     shapesArr.push(shapes.cake, shapes.party_face);
   }
-  if (text.includes("linux")) {
-    shapesArr.push(shapes.penguin);
-  }
-  if (text.includes("lemmy")) {
-    shapesArr.push(shapes.mouse);
-  }
-  if (text.includes("piefed")) {
-    shapesArr.push(shapes.pie);
-  }
+  // Todo: add other special phrases
   if (shapesArr.length > 0) {
     canvasConfetti({
       ...CONFIG,
