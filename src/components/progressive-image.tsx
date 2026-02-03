0c1d91e8b29d3f38fea726a7b166452e0c3df5ba
Christian <5768801+christianjuth@users.noreply.github.com>
2026-02-02 23:01:15 -0500

    fix: image flicker

--- src/components/progressive-image.tsx
+++ src/components/progressive-image.tsx
@@ -109,7 +109,7 @@ export const ProgressiveImage: React.FC<ProgressiveImageProps> = ({
           onLoad?.();
         }}
         onError={() => onError?.()}
-        className={cn("w-full h-full", imgClassName, hiVisible && "opacity-0")}
+        className={cn("w-full h-full", imgClassName)}
       />
     </div>
   );
