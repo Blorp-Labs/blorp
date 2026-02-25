709d89174eddffcbc0ac036f4bac8e960223e2e8
Christian <5768801+christianjuth@users.noreply.github.com>
2026-02-25 17:00:22 -0500

    refactor: increase size of show NSFW post button

--- src/components/posts/nsfw-blur-toggle.tsx
+++ src/components/posts/nsfw-blur-toggle.tsx
@@ -1,15 +1,14 @@
 import { Button } from "../ui/button";
-import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 
 export function NsfwBlurToggle({ onClick }: { onClick: () => void }) {
   return (
-    <Tooltip>
-      <TooltipTrigger className="absolute top-1/2 left-1/2 -translate-x-1/2">
-        <Button variant="ghost" size="lg" className="text-lg" onClick={onClick}>
-          NSFW
-        </Button>
-      </TooltipTrigger>
-      <TooltipContent>Remove NSFW blur</TooltipContent>
-    </Tooltip>
+    <Button
+      variant="secondary"
+      size="lg"
+      className="text-xl absolute top-1/2 left-1/2 -translate-x-1/2 p-8"
+      onClick={onClick}
+    >
+      Show NSFW
+    </Button>
   );
 }
