8a4f204a61017a863b3f65f47324b702a3c4e740
Christian <5768801+christianjuth@users.noreply.github.com>
2026-03-02 21:21:00 -0500

    refactor: pr cleanup

--- src/features/site-modlog.tsx
+++ src/features/site-modlog.tsx
@@ -68,7 +68,6 @@ export default function SiteModlog() {
           renderItem={({ item }) => (
             <ContentGutters noMobilePadding>
               <ModlogRow item={item} />
-              <></>
             </ContentGutters>
           )}
         />
