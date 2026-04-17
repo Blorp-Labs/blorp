c6c3ae374005d3252c857ccd3319efdc609b8f51
Christian <5768801+christianjuth@users.noreply.github.com>
2026-04-17 00:25:48 -0400

    chore: add comment

--- src/hooks/use-event.ts
+++ src/hooks/use-event.ts
@@ -1,5 +1,8 @@
 import { useCallback, useLayoutEffect, useRef } from "react";
 
+// Potentially we might want to replace this with
+// https://reactuse.com, which has this and a bunth of
+// other helpful hooks
 export function useEvent<T extends (...args: any[]) => any>(fn: T): T {
   const ref = useRef(fn);
   useLayoutEffect(() => {
